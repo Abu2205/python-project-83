71252414ecdc3061509cbb9094dac1d52ac6f07d
Abu <abylaikhanturmanov@gmail.com>
2025-05-27 16:51:51 +0000

    fixed build.sh for correct deploy 1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
-which uv || { echo "uv not found in PATH"; exit 1; }
-make install && psql -a -d $DATABASE_URL -f database.sql
+
+make install
